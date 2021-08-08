c5fd6f649666ae70cf79b665aa9a2d306d98277f
kim-shun <kimura1417080@gmail.com>
2021-08-08 22:38:03 +0900

    privateなメソッドを定義

--- Chapter4_5/src/example/Cat.java
+++ Chapter4_5/src/example/Cat.java
@@ -5,9 +5,13 @@ public class Cat {
 	int age;
 	boolean hungry;
 	
+	private void printMessage(String message) {
+		System.out.println(name + "> " + message);
+	}
+	
 	public void eat() {
-		System.out.println(name + ">" + "ご飯を食べるよ！おいしいにゃー");
-		System.out.println(name + "> お腹が一杯になったにゃー");
+		printMessage("ご飯を食べるよ！おいしいにゃー");
+		printMessage("お腹が一杯になったにゃー");
 		hungry = false;
 	}
 	
@@ -16,8 +20,8 @@ public class Cat {
 	}
 	
 	public void playToy(String toy) {
-		System.out.println(name + ">" + toy + "で遊ぶよ。楽しいにゃー");
-		System.out.println(name + "> 遊んでお腹が減ったにゃー");
+		printMessage(toy + "で遊ぶよ。楽しいにゃー");
+		printMessage("遊んでお腹が減ったにゃー");
 		hungry = true;
 	}
 	
@@ -38,9 +42,7 @@ public class Cat {
 	}
 	
 	public void introduceMyself() {
-		String n = getName();
-		int a = getAge();
-		System.out.println("名前は" + n + "です、" + a + "歳です。");
+		printMessage("名前は" + getName() + "です、" + getAge() + "歳です。");
 	}
 
 }
--- Chapter4_5/src/example2/PublicAccessSample.java
+++ Chapter4_5/src/example2/PublicAccessSample.java
@@ -7,9 +7,11 @@ public class PublicAccessSample {
 	public static void main(String[] args) {
 		Cat tama = new Cat();
 		tama.setName("タマ");
-		System.out.println(tama.getName());
 		tama.setAge(3);
-		System.out.println(tama.getAge());
+		
+		tama.eat();
+		tama.playToy("ボール");
+		tama.introduceMyself();
 
 	}
 
