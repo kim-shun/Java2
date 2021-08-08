e358c9ebc623731cdbe613ff35d72c415b1ed7b7
kim-shun <kimura1417080@gmail.com>
2021-08-08 20:48:38 +0900

    Catクラスのメソッドを全てpublicにする

--- Chapter4_5/src/example/Cat.java
+++ Chapter4_5/src/example/Cat.java
@@ -5,39 +5,39 @@ public class Cat {
 	int age;
 	boolean hungry;
 	
-	void eat() {
+	public void eat() {
 		System.out.println(name + ">" + "ご飯を食べるよ！おいしいにゃー");
 		System.out.println(name + "> お腹が一杯になったにゃー");
 		hungry = false;
 	}
 	
-	boolean isHungry() {
+	public boolean isHungry() {
 		return true;
 	}
 	
-	void playToy(String toy) {
+	public void playToy(String toy) {
 		System.out.println(name + ">" + toy + "で遊ぶよ。楽しいにゃー");
 		System.out.println(name + "> 遊んでお腹が減ったにゃー");
 		hungry = true;
 	}
 	
-	void setName(String catName) {
+	public void setName(String catName) {
 		name = catName;
 	}
 	
-	String getName() {
+	public String getName() {
 		return name;
 	}
 	
-	void setAge(int catAge) {
+	public void setAge(int catAge) {
 		age = catAge;
 	}
 	
-	int getAge() {
+	public int getAge() {
 		return age;
 	}
 	
-	void introduceMyself() {
+	public void introduceMyself() {
 		String n = getName();
 		int a = getAge();
 		System.out.println("名前は" + n + "です、" + a + "歳です。");
