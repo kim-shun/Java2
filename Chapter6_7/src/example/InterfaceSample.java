6d6dd7acab09e25d9369827ac13086b6f15f37ed
kim-shun <kimura1417080@gmail.com>
2021-08-09 12:19:13 +0900

    インターフェースを実装したキャットクラスを利用する

---
+++ Chapter6_7/src/example/InterfaceSample.java
@@ -0,0 +1,16 @@
+package example;
+
+public class InterfaceSample {
+
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		
+		cat.eat();
+		cat.eat("贅沢なキャットフード");
+		
+		cat.playToy();
+		cat.playToy("猫じゃらし");
+
+	}
+
+}
