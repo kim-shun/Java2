53259ed1eb65bff8d36407be969c51df3bda53b2
kim-shun <kimura1417080@gmail.com>
2021-08-09 12:24:33 +0900

    Dogクラスを作成

---
+++ Chapter6_7/src/example/Dog.java
@@ -0,0 +1,31 @@
+package example;
+
+public class Dog implements Pet {
+
+	@Override
+	public void eat() {
+		eat("ご飯");
+
+	}
+
+	@Override
+	public void eat(String food) {
+		System.out.println(food + "を食べるよ！美味しいワン");
+		System.out.println("お腹が一杯になったワン");
+
+	}
+
+	@Override
+	public void playToy() {
+		playToy("おもちゃ");
+
+	}
+
+	@Override
+	public void playToy(String toy) {
+		System.out.println(toy + "で遊ぶよ！楽しいワン");
+		System.out.println("遊んでお腹が減ったワン");
+
+	}
+
+}
