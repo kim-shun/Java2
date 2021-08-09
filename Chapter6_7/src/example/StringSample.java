0f9b47ce670ed5c5390326c7f57d993fefd4600d
kim-shun <kimura1417080@gmail.com>
2021-08-09 15:21:13 +0900

    Stringクラスのメソッド

---
+++ Chapter6_7/src/example/StringSample.java
@@ -0,0 +1,17 @@
+package example;
+
+public class StringSample {
+
+	public static void main(String[] args) {
+		String str1 = "123";
+		System.out.println(str1.length());
+		
+		String str3 = "ABCdef";
+		System.out.println(str3.toLowerCase());
+		
+		String str4 = "ABCdef123ＡＢＣｄｅｆ";
+		System.out.println(str4.toUpperCase());
+
+	}
+
+}
