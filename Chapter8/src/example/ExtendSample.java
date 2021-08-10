3c89a132dd38a76a12f49c63b02a7834fc3ff2ff
kim-shun <kimura1417080@gmail.com>
2021-08-10 22:05:47 +0900

    Catクラスを利用するクラスの作成

---
+++ Chapter8/src/example/ExtendSample.java
@@ -0,0 +1,15 @@
+package example;
+
+public class ExtendSample {
+
+	public static void main(String[] args) {
+		Cat tama = new Cat("タマ", 3);
+		
+		tama.eat();
+		tama.introduceMyself();
+		tama.setAge(4);
+		tama.introduceMyself();
+
+	}
+
+}
