bad5318827d4f7ac55d6baa36de71032b3e4bb80
kim-shun <kimura1417080@gmail.com>
2021-08-10 15:18:57 +0900

    リストからインスタンスを取り出し、メソッドを実行する

--- Chapter6_7/src/example/ListSample2.java
+++ Chapter6_7/src/example/ListSample2.java
@@ -12,6 +12,17 @@ public class ListSample2 {
 		Cat mike = new Cat();
 		catList.add(tama);
 		catList.add(mike);
+		
+		Cat cat1 = catList.get(0);
+		Cat cat2 = catList.get(1);
+		
+		cat1.eat();
+		cat1.playToy();
+		
+		System.out.println("---------------------------");
+		
+		cat2.eat();
+		cat2.playToy();
 
 	}
 
