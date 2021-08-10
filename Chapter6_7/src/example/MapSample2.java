e4225adff98cdd07c1a043e3b9e4d55ca0d0278b
kim-shun <kimura1417080@gmail.com>
2021-08-10 16:46:16 +0900

    マップに所有者とペットをセットにして追加する

---
+++ Chapter6_7/src/example/MapSample2.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSample2 {
+
+	public static void main(String[] args) {
+		Map<String, Pet> petMap = new HashMap<>();
+		
+		Pet pet1 = new Cat();
+		Pet pet2 = new Dog();
+		
+		petMap.put("佐藤さん", pet1);
+		petMap.put("鈴木さん", pet2);
+		petMap.put("田中さん", new Cat());
+		
+		System.out.println("鈴木さんのペットにご飯をあげます");
+		Pet petOwnerdBySuzuki = petMap.get("鈴木さん");
+		petOwnerdBySuzuki.eat();
+		
+		Pet petOwnerdByTanaka = petMap.get("田中さん");
+		petOwnerdByTanaka.eat();
+		
+		Pet petOwnerdBySato = petMap.get("佐藤さん");
+		petOwnerdBySato.eat();
+
+	}
+
+}
