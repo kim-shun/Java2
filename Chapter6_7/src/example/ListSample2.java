86325c84ea6215e95e869beb856856590c098539
kim-shun <kimura1417080@gmail.com>
2021-08-10 15:15:56 +0900

    Cat型のインスタンスをリストに追加する

---
+++ Chapter6_7/src/example/ListSample2.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSample2 {
+
+	public static void main(String[] args) {
+		List<Cat> catList = new ArrayList<>();
+		
+		Cat tama = new Cat();
+		Cat mike = new Cat();
+		catList.add(tama);
+		catList.add(mike);
+
+	}
+
+}
