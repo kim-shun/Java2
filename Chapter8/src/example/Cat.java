db1f9146dc925a345c53672a3e748292a5b881b8
kim-shun <kimura1417080@gmail.com>
2021-08-10 22:18:13 +0900

    Petインターフェースを実装したクラスにする

--- Chapter8/src/example/Cat.java
+++ Chapter8/src/example/Cat.java
@@ -1,28 +1,32 @@
 package example;
 
-public class Cat extends Animal {
+public class Cat extends Animal implements Pet {
 	public Cat(String name, int age) {
 		setName(name);
 		setAge(age);
 		System.out.println(name + "," + age);
 	}
 		
+	@Override
 	public void eat() {
 		eat("ご飯");
 	}
 	
+	@Override
 	public void eat(String food) {
 		printMessage(food + "を食べるよ！美味しいにゃー");
 		printMessage("お腹が一杯になったにゃー");
 		setHungry(false);
 	}
 	
+	@Override
 	public void playToy(String toy) {
 		printMessage(toy + "で遊ぶよ。楽しいにゃー");
 		printMessage("遊んでお腹が一杯になったにゃー");
 		setHungry(true);
 	}
 	
+	@Override
 	public void playToy() {
 		playToy("おもちゃ");
 	}
--- Chapter8/src/example/Dog.java
+++ Chapter8/src/example/Dog.java
@@ -1,28 +1,32 @@
 package example;
 
-public class Dog extends Animal {
+public class Dog extends Animal implements Pet {
 	public Dog(String name, int age) {
 		setName(name);
 		setAge(age);
 		System.out.println(name + "," + age);
 	}
 	
+	@Override
 	public void eat() {
 		eat("ご飯");
 	}
 	
+	@Override
 	public void eat(String food) {
 		printMessage(food + "を食べるよ！美味しいワン");
 		printMessage("お腹が一杯になったワン");
 		setHungry(false);
 	}
 	
+	@Override
 	public void playToy(String toy) {
 		printMessage(toy + "で遊ぶよ。楽しいワン");
 		printMessage("遊んでお腹が減ったワン");
 		setHungry(true);
 	}
 	
+	@Override
 	public void playToy() {
 		playToy("おもちゃ");
 	}
